d8a391c46f424e7e22008f7419ab09faaa392e63
Abdelrahman <abdofathi317@gmail.com>
2023-09-25 12:53:04 -0700

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,15 +6,15 @@
  * @src: pointer to char params
  * @n: size
  * Return: *dest
-*/
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-dest[i] = *src;
-src++;
-}
-return (dest);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
