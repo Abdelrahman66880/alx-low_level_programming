3fe431c00a7e5f515cc765cf42b8b25b72cac86d
Abdelrahman <abdofathi317@gmail.com>
2023-10-05 05:00:47 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+(void) argv;
+return (0);
+}
