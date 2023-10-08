3edc1a30208e4ee8e2f4b4961d2159cb6a0d21a2
Abdelrahman <abdofathi317@gmail.com>
2023-10-08 10:36:45 -0700

    312

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,24 +5,22 @@
  *@nmemb: number of element in the array
  *@size: bytes for each position in array
  *Return: pointer void
-*/
-
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *ptr;
-int i;
+char *p;
+unsigned int i;
+
 if (nmemb == 0 || size == 0)
-{
 return (NULL);
-}
-ptr = malloc(nmemb * sizeof(char));
-if (ptr == NULL)
-{
+
+p = malloc(nmemb * size);
+if (p == NULL)
 return (NULL);
-}
-for (i = 0; i < nmemb; i++)
-{
-ptr[i] = 0;
-}
-return (ptr);
+
+for (i = 0; i < nmemb * size; i++)
+p[i] = 0;
+
+return (p);
+
 }
