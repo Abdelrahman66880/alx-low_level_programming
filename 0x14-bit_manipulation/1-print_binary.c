19a534d8a2cc9a4d5e9572faeab7edbc840e68db
Abdelrahman <abdofathi317@gmail.com>
2023-10-26 13:39:41 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,19 +12,19 @@ int flag = 0;
 int bits = sizeof(unsigned long int) * 8;
 if (n == 0)
 {
-putchar('0');
+printf('0');
 }
 
 for (i = bits - 1; i >= 0; i--)
 {
 if ((n >> i) & 1)
 {
-putchar('1');
+printf('1');
 flag = 1;
 }
 else if (flag)
 {
-putchar('0');
+printf('0');
 }
 }
 }
