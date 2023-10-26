bc7ad3c1a32b6289d5df41392f39462d7348e29d
Abdelrahman <abdofathi317@gmail.com>
2023-10-26 13:37:19 -0700

    my commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+int i;
+int flag = 0;
+int bits = sizeof(unsigned long int) * 8;
+if (n == 0)
+{
+putchar('0');
+}
+
+for (i = bits - 1; i >= 0; i--)
+{
+if ((n >> i) & 1)
+{
+putchar('1');
+flag = 1;
+}
+else if (flag)
+{
+putchar('0');
+}
+}
+}
