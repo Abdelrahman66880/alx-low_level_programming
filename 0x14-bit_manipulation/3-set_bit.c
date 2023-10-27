97376746143091b1a60ebb8a95425ac10524d6b1
Abdelrahman <abdofathi317@gmail.com>
2023-10-27 09:34:24 -0700

    3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,16 +6,11 @@
  * @index: index of the bit to set to 1
  *
  * Return: 1 for success, -1 for failure
-*/
-
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-int set;
 if (index > 63)
-{
 return (-1);
-}
 *n = ((1UL << index) | *n);
-return (*n);
 return (1);
 }
