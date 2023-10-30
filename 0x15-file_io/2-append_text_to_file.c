d5d9bf91728e9e10e762f81e41648db85b3fe52f
Abdelrahman <abdofathi317@gmail.com>
2023-10-30 16:17:19 -0700

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,28 +8,27 @@
  * Return: If the function fails or filename is NULL - -1.
  *         If the file does not exist the user lacks write permissions - -1.
  *         Otherwise - 1.
-*/
-
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int o, w, len = 0;
-int i;
-if (filename == NULL)
-{
-return (0);
-}
-if (text_content == NULL)
-{
-return (0);
-}
-for (i = 0; text_content[i] != '\0'; i++)
-{
-len++;
-}
-o = open(filename, O_WRONLY | O_APPEND);
-w = write(o, text_content, len);
-if (o == -1 || w == -1)
-return (-1);
-close(o);
-return (1);
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
